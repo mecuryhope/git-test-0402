fc547377f101e5fa508176565a19c0882df2a24f
menglili <mecuryhope@qq.com>
2019-04-03 12:02:51 +0800

    Initial commit

---
+++ src/main/java/com/test/springboot/gittest0402/TcmClientBackendApplication.java
@@ -0,0 +1,13 @@
+package com.test.springboot.gittest0402;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcmClientBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcmClientBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/springboot/gittest0402/TcmClientBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.test.springboot.gittest0402;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TcmClientBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
